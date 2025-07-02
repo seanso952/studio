422f635f11500cd4b7497918e28417ca33b0abb8
Sean So <seanso952@gmail.com>
2025-07-02 15:44:05 +0000

    The file in the estatemind seems redundant since it is already present i

--- C:\dev\estatemind-project\src\ai\dev.ts
+++
@@ -1,5 +0,0 @@
-
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/contract-time-analysis.ts';
--- C:\dev\estatemind-project\src\app\(app)\layout.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function AppLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <>{children}</>;
-}
